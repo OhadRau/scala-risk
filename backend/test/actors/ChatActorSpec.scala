e5bcc5e6fd3d2d08e7728bba35a51c87b68a22e6
oswinso <oswinso@gmail.com>
2019-02-24 12:37:12 -0500

    Fixed nondeterministic tests for chatting

--- backend/test/actors/ChatActorSpec.scala
+++ backend/test/actors/ChatActorSpec.scala
@@ -43,7 +43,6 @@ class ChatActorSpec extends TestKitSpec with GivenWhenThen {
       client.expectMsgPF() {
         case NotifyClientsChanged(clientsSeq: Seq[ClientBrief]) =>
           clientsSeq exists (clientBrief => clientBrief.name == "A") should be (true)
-          publicToken(0) = clientsSeq.head.publicToken
       }
     })
     rootActor ! AssignName("B", tokens(1))
@@ -52,26 +51,32 @@ class ChatActorSpec extends TestKitSpec with GivenWhenThen {
         case NotifyClientsChanged(clientsSeq: Seq[ClientBrief]) =>
           clientsSeq exists (clientBrief => clientBrief.name == "A") should be (true)
           clientsSeq exists (clientBrief => clientBrief.name == "B") should be (true)
-          publicToken(1) = if (clientsSeq.head.publicToken != publicToken(0)) clientsSeq.head.publicToken else clientsSeq(1).publicToken
+          clientsSeq foreach { brief =>
+            if (brief.name == "A") publicToken(0) = brief.publicToken
+            if (brief.name == "B") publicToken(1) = brief.publicToken
+          }
       }
     })
 
     Then("B can send a message to A using A's public token")
-    var message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare ut arcu non venenatis. Donec nec suscipit turpis, ac congue"
-    rootActor ! MessageToUser(tokens(1), publicToken(1), message)
-    clients(0).fishForSpecificMessage(){case UserMessage("B", message) => }
-    clients(1).fishForSpecificMessage(){case UserMessage("B", message) => }
+    val message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare ut arcu non venenatis. Donec nec suscipit turpis, ac congue"
+    rootActor ! MessageToUser(tokens(1), publicToken(0), message)
+    logger.debug("0")
+    clients(1).expectMsg(UserMessage("B", message))
+    logger.debug("1")
+    clients(0).expectMsg(UserMessage("B", message))
+    logger.debug("2")
   }
   it should "be able to send and receive a lot of messages" in {
     val r = new Random
-    for (i <- 0 until 100) {
-      var sender = r.nextInt(1)
-      var name = if(sender == 0) "A" else "B"
-      var message = Random.alphanumeric take 20 mkString
+    for (i <- 0 until 100000) {
+      val sender = r.nextInt(1)
+      val name = if(sender == 0) "A" else "B"
+      val message = Random.alphanumeric take 20 mkString
 
-      rootActor ! MessageToUser(tokens(sender), publicToken(sender), message)
-      clients(0).expectMsg(10 seconds, UserMessage(name, message))
-      clients(1).expectMsg(10 seconds, UserMessage(name, message))
+      rootActor ! MessageToUser(tokens(sender), publicToken(-sender+1), message)
+      clients(0).expectMsg(UserMessage(name, message))
+      clients(1).expectMsg(UserMessage(name, message))
     }
   }
 }
